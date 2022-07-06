1e7f4b9539ae1e4d1001bf1952bc7634d9f7be6a
Tory <88531407+1two13@users.noreply.github.com>
2022-07-06 18:05:42 +0900

    feature: Top 컴포넌트 생성(제일 상단부 ~ 검색창)

---
+++ src/components/Top.js
@@ -0,0 +1,57 @@
+import styled from "styled-components";
+import { useState } from "react";
+
+const Title = styled.h1`
+  margin-bottom: 10px;
+`;
+
+const ChangeMovie = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  cursor: pointer;
+  color: #666666;
+`;
+
+const Span = styled.span`
+  margin-right: 8px;
+  &.changeColor {
+    color: #ff4256;
+    font-weight: 500;
+  }
+`;
+
+const Hr = styled.hr`
+  border: solid;
+  margin: 3px 0px 7px 0px;
+`;
+
+const Input = styled.input`
+  width: 30%;
+  margin-bottom: 7px;
+`;
+
+function Top() {
+  const [color, setColor] = useState(true);
+
+  const onClickHandler = () => {
+    setColor(!color);
+  };
+
+  return (
+    <div>
+      <Title>무비 스케쥴러</Title>
+      <ChangeMovie>
+        <Span className={color ? "changeColor" : ""} onClick={onClickHandler}>
+          무비 차트
+        </Span>
+        <Span className={color ? "" : "changeColor"} onClick={onClickHandler}>
+          개봉 예정작
+        </Span>
+      </ChangeMovie>
+      <Hr />
+      <Input type="text" />
+    </div>
+  );
+}
+
+export default Top;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import Top from "./components/Top";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+    <Top />
   </React.StrictMode>
 );
